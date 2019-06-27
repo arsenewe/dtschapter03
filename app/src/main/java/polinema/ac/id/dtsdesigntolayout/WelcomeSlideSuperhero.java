6eed72164ebab9ac9b2e3bb9a98d78cee73242ba
Putra Prima Arhandi <putraprima@gmail.com>
2019-06-28 06:16:45 +0700

    tambah layout baru

--- app/src/main/java/polinema/ac/id/dtsdesigntolayout/WelcomeSlideSuperhero.java
+++ app/src/main/java/polinema/ac/id/dtsdesigntolayout/WelcomeSlideSuperhero.java
@@ -3,11 +3,11 @@ package polinema.ac.id.dtsdesigntolayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class WelcomeSlide extends AppCompatActivity {
+public class WelcomeSlideSuperhero extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_slide);
+        setContentView(R.layout.activity_welcome_slide_superhero);
     }
 }
